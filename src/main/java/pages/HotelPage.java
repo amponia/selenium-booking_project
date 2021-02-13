308b1e90e6d5cc01452748ffee2baa7196a7f48d
amponia <ka.zielinska@gmail.com>
2021-02-13 20:41:41 +0100

    adding gotoreservationbutton & attempt to write loop for select a room

--- src/main/java/pages/HotelPage.java
+++ src/main/java/pages/HotelPage.java
@@ -5,26 +5,49 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class HotelPage extends PageObject {
 
-    @FindBy (tagName = "h2")
+    @FindBy(tagName = "h2")
     private WebElement hotelNameHeader;
 
-    @FindBy (id = "b_tt_holder_1")   //previous: bui-button__text
-    private WebElement reservationButton;
+    @FindBy(xpath = "//*[@id = \"hp_book_now_button\"]")
+    private WebElement goToReservationButton;
+
+    @FindBy(className = "hprt-roomtype-link")
+    private List<WebElement> accomodationType;
+
+    @FindBy(xpath = "//class[@contains = \"hprt-roomtype-icon-link\" ]")
+    WebElement selectNumberOfBedsButton;
+
+    @FindBy(className = "bui-price-display__value")
+    WebElement roomPrice;
+
+    @FindBy(className = "hprt-reservation-cta")
+    WebElement reservationButton;
 
     public HotelPage(WebDriver driver, String hotelName) {
         super(driver);
 //        wait.until(ExpectedConditions.textToBePresentInElement(hotelNameHeader, hotelName));
     }
 
-    public ReservationDataPage clickReservationButton () {
+    public void clickReservationButton() {
         wait.until(ExpectedConditions.elementToBeClickable(reservationButton));
-        reservationButton.click();
+        goToReservationButton.click();
 
-        return new ReservationConfirmationPage(driver);
     }
 
-
+    public void selectNumberOfBedsInChosenRoom(String roomName, int availableBudget) {
+        for (WebElement element : accomodationType) {
+            if ((element.toString()).contains(roomName)) {
+                if ((Integer.parseInt(String.valueOf(roomPrice))) > availableBudget) {
+                    Select numberOfBeds = new Select(selectNumberOfBedsButton);
+                    numberOfBeds.selectByIndex(2);
+                }
+            }
+        }
+    }
 }
--- src/test/java/HotelPageTest.java
+++ src/test/java/HotelPageTest.java
@@ -1,5 +1,6 @@
 import config.WebDriverFactory;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import pages.*;
 
@@ -14,21 +15,21 @@ public class HotelPageTest {
         WebDriver driver = WebDriverFactory.CHROME.create();
         driver.get("https://booking.com");
         HomePage homePage = new HomePage(driver);
-        homePage.acceptCookies();
+//        homePage.acceptCookies();
         homePage.enterCity("Warszawa");
         homePage.clickCheckInButton();
-//        select CheckInDate do Cucumbera
         homePage.selectCheckInDate(LocalDate.of(2021, 2, 27));
-//        select CheckInDate bez Cucumbera
-//        homePage.selectCheckInDate(LocalDate.of(2021, 2, 5));
         homePage.clickCheckOutButton();
         homePage.clickCheckOutButton();
         homePage.selectCheckOutDate(LocalDate.of(2021, 2, 28));
-        HotelListPage hotelListPage = homePage.clickSearchButton("Warszawa");
-        HotelPage hotelPage = hotelListPage.clickAvailabilityByHotelName("GO");
+        HotelListPage hotelListPage = homePage.clickSearchButton("Warsaw");
+        HotelPage hotelPage = hotelListPage.clickAvailabilityByHotelName("Novotel");
         List<String> tabs = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(1));
-        ReservationDataPage reservationDataPage = hotelPage.clickReservationButton();
+        hotelPage.clickReservationButton();
+        hotelPage.selectNumberOfBedsInChosenRoom("Superior", 50);
+
+
 
 
     }
